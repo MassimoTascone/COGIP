78d26981d454c20273772c2070f8437abfaad70c
Massimo Tascone <tascone.massimo@gmail.com>
2019-10-28 11:37:37 +0100

    db

--- COGIP.sql
+++ COGIP.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: database:3306
--- Generation Time: Oct 28, 2019 at 08:56 AM
+-- Generation Time: Oct 28, 2019 at 10:36 AM
 -- Server version: 10.4.2-MariaDB-1:10.4.2+maria~bionic
 -- PHP Version: 7.2.5
 
@@ -25,19 +25,67 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `TVA` varchar(50) NOT NULL,
+  `country` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `id` int(11) NOT NULL,
+  `invoice_number` int(255) NOT NULL,
+  `invoice_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `people`
 --
 
 CREATE TABLE `people` (
   `id` int(11) NOT NULL,
-  `first_name` int(11) NOT NULL,
-  `last_name` int(11) NOT NULL,
-  `email` int(11) NOT NULL
+  `first_name` text NOT NULL,
+  `last_name` text NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type_company`
+--
+
+CREATE TABLE `type_company` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `people`
@@ -46,13 +94,37 @@ ALTER TABLE `people`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `type_company`
+--
+ALTER TABLE `type_company`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `people`
 --
 ALTER TABLE `people`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `type_company`
+--
+ALTER TABLE `type_company`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
