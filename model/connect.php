e8dd00bc63cf11cdc68fc81f69af4c5750daf5a6
Massimo Tascone <tascone.massimo@gmail.com>
2019-10-29 15:00:20 +0100

    ok

--- model/connect.php
+++ model/connect.php
@@ -1,18 +1,7 @@
 <?php
 // Connection to DB
 
-
-
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-$conn = new PDO('mysql:host=localhost;dbname=COGIP', 'root', '');
-=======
 $conn = new PDO('mysql:host=database;dbname=COGIP', 'root', 'root');
->>>>>>> 0cf6affb3cba6960a9ceb79db3f719b9441e2228
-=======
-$conn = new PDO('mysql:host=database;dbname=COGIP', 'root', 'root');
->>>>>>> massimo
 
 try {
     $conn;
