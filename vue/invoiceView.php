dbdc2e49b60e9e0d3d9a66aff62fdca8e4a34551
arturo <artedsolis@gmail.com>
2019-11-04 16:28:24 +0100

    rangement de l'archi MVC

--- vue/invoiceView.php
+++ vue/invoiceView.php
@@ -1,9 +1,13 @@
 <?php
-
-require('model/connect.php');
-require('controller/controller.php');
-
+require('model/invoiceModel.php');
+require('controller/invoiceController.php');
 // on affiche nos donées
-echo "<table class='table'>";
+echo "<table class='table'>
+<th>Id</th>
+<th>Number</th>
+<th>Date</th>
+<th>Company</th>
+<th>Country</th>;
+<th>Type</th>";
 createTable($invoice);
 echo "</table>";
