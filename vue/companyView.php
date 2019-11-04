f93b823c678ca9f149413b306cf343d46ff3b0c3
Greg <g.wastelain@gmail.com>
2019-11-04 14:57:45 +0100

    companyView séparaton client et fournisseurs

--- vue/companyView.php
+++ vue/companyView.php
@@ -2,10 +2,12 @@
 require('model/connect.php');
 require('controller/controller.php');
 
+echo "<h1> Clients </h1>";
 echo "<table class='table'>";
 createTable($companyClient);
 echo "</table>";
 
+echo "<h1> Fournisseurs </h1>";
 echo "<table class='table'>";
 createTable($companyFournisseur);
 echo "</table>";
