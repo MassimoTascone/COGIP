8ac82bf86f0b3770389645fdfa9b6317d39c921a
arturo <artedsolis@gmail.com>
2019-11-05 10:21:02 +0100

    changing views name folder

--- vue/peopleView.php
+++
@@ -1,9 +0,0 @@
-<h1>Contact List</h1>
-<table class='table'>
-    <th>Id</th>
-    <th>Prénom</th>
-    <th>Nom</th>
-    <th>E-mail</th>
-    <th>Société</th>
-    <?php createTable($people); ?>
-</table>
